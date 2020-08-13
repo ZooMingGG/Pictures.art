2ff8725e87a95cf43e245e9c541390a62f16e93d
Volodymyr Lukashevych <volodia.lukashewwich@gmail.com>
2020-08-13 13:41:53 +0300

    add media-query for main slider

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     modals();
+    
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
 
